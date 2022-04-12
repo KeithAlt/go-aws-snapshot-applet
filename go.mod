2b44c685ddf574c0112c1d7d88df9c51e3be8f5c
Keith <claymorebusinessinquiry@gmail.com>
2022-04-12 16:05:23 -0400

    Added app files

---
+++ go.mod
@@ -0,0 +1,15 @@
+module go-aws-snapshot-applet
+
+go 1.17
+
+require (
+	github.com/aws/aws-sdk-go v1.43.37
+	github.com/joho/godotenv v1.4.0
+)
+
+require (
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	golang.org/x/net v0.0.0-20220407224826-aac1ed45d8e3 // indirect
+	golang.org/x/text v0.3.7 // indirect
+)
---
+++ main.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/rds"
+	"github.com/joho/godotenv"
+	"log"
+	"os"
+	"time"
+)
+
+// Initialize our env variables
+func initEnv() error {
+	log.Println("Initializing environment variables...")
+
+	loadErr := godotenv.Load("./config/.env")
+	if loadErr != nil {
+		return loadErr
+	}
+
+	if os.Getenv("AWS_DB_ID") == "<INSERT_HERE>" || os.Getenv("AWS_CONFIG_REGION") == "<INSERT_HERE>" {
+		return errors.New("configurationFailure: your /config/.env file isn't configured; do so then restart this app")
+	}
+
+	log.Println("Successfully initialized environment variables")
+	return nil
+}
+
+// Create our AWS session
+func createSession() *session.Session {
+	log.Println("Establishing AWS session...")
+
+	Session := session.Must(session.NewSession(&aws.Config{
+		Region: aws.String(os.Getenv("AWS_CONFIG_REGION")),
+	}))
+
+	log.Println("Successfully established AWS session")
+	return Session
+}
+
+// Create our database snapshot
+func createSnapshot(session *session.Session) (*rds.CreateDBSnapshotOutput, error) {
+	curTime := time.Now()
+	dbID := "snapshot-backup-" + curTime.Format("2006-01-02-at-15-04")
+
+	log.Println("Attempting to create new database backup: ", dbID, "...")
+
+	svc := rds.New(session)
+	DBInstanceIdentifier := os.Getenv("AWS_DB_ID")
+	DBSnapshotIdentifier := dbID
+	dbSnapshotReq := rds.CreateDBSnapshotInput{
+		DBInstanceIdentifier: &DBInstanceIdentifier,
+		DBSnapshotIdentifier: &DBSnapshotIdentifier,
+	}
+
+	validateErr := dbSnapshotReq.Validate()
+	if validateErr != nil {
+		return nil, validateErr
+	}
+
+	res, resErr := svc.CreateDBSnapshot(&dbSnapshotReq)
+	if resErr != nil {
+		return nil, resErr
+	}
+
+	log.Println("Successfully created database snapshot ", dbID)
+	return res, nil
+}
+
+// Our driver
+func main() {
+	log.Println("Starting remote database backup...")
+
+	initErr := initEnv()
+	if initErr != nil {
+		log.Fatal(initErr)
+	}
+
+	Session := createSession()
+	_, resErr := createSnapshot(Session)
+	if resErr != nil {
+		fmt.Println("resErr took place")
+		log.Fatal(resErr)
+	}
+
+	log.Println("Completed remote database backup")
+}
